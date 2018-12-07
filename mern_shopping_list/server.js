cdcefb3d961fbe775c1c15ab8d1799b9834f83de
Andrew Miracle <me@andrewmiracle.com>
2018-12-07 11:32:11 +0200

    add modules to project

--- mern_shopping_list/server.js
+++ mern_shopping_list/server.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+
 
