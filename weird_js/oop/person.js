65641bff8bf12b543c89ad55f9fcda174c795a2a
Andrew Miracle <me@andrewmiracle.com>
2019-05-11 21:37:32 +0100

    javascript primitives

--- weird_js/oop/person.js
+++ weird_js/oop/person.js
@@ -9,3 +9,14 @@ console.log(me)
 
 const person2 = new Person('Clancey', 'Turner', 51)
 console.log(person2)
+
+console.log(new String)
+
+
+/// Primitives in Javascript 
+// Aside from null, undefined and NaN, every other js primitive is a subset of Objects.prototype in javascript
+
+// Object ---> Object.prototype ---> null
+// Array ---> Array.prototype ---> Object.prototype ---> null
+// String ---> String.prototype ---> Object.prototype ---> null
+// Number ---> Number.prototype ---> Object.prototype ---> null
